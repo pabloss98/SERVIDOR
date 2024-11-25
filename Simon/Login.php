a6cdf0f5e9852b06becae06293805659ef78d538
pabloss98 <03.pablo.ss@gmail.com>
2024-11-25 14:26:50 +0100

    commit

---
+++ Simon/Login.php
@@ -0,0 +1,6 @@
+<?php // login.php
+ $hn = 'localhost';
+ $db = 'bede_simon';
+ $un = 'Jugador';
+ $pw = 'Jugador';
+?> 
---
+++ Simon/Optencionresul.php
@@ -0,0 +1,19 @@
+<?php // query-mysqli.php
+    require_once 'login.php';
+    $connection = new mysqli($hn, $un, $pw, $db);
+        if ($connection->connect_error) die("Fatal Error");
+            $query = "SELECT usu, contraseña FROM usuarios";
+            $result = $connection->query($query);
+        if (!$result) die("Fatal Error");
+            $rows = $result->num_rows;
+
+        for ($j = 0 ; $j < $rows ; ++$j){
+
+
+            $result->data_seek($j);
+            echo 'Usuario: ' .htmlspecialchars($result->fetch_assoc()['usu']) .'<br>';
+
+        }
+ $result->close();
+ $connection->close();
+?> 
