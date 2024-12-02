dd8817dae77674edd0b03e4e551f3c132a143532
Pablo Suárez Sánchez <03.pablo.ss@gmail.com>
2024-12-02 21:40:13 +0100

    Commit

--- Simonexa/acierto.php
+++ Simonexa/acierto.php
@@ -15,6 +15,6 @@ $usuario = $_SESSION['usuario'];?>
         include "pintar-circulos.php";
         pintar_circulos($_SESSION['resp1'],$_SESSION['resp2'],$_SESSION['resp3'],$_SESSION['resp4']);
     ?>
-    <a href="index.php">Volver a jugar</a>
+    <a href="inicio.php">Volver a jugar</a>
 </body>
 </html>
