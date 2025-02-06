3d74781dfaad0137e27aaf0b2ffb36cda2f1f4d5
Pablo Suárez <03.pablo.ss@gmail.com>
2025-02-06 14:23:13 +0100

    Commit

--- MIVIDEOCLUB/routes/web.php
+++ MIVIDEOCLUB/routes/web.php
@@ -11,6 +11,16 @@ Route::get('pagina1', function () {
     return'Estás en la pagina 1';
  });
 
- Route::get('pagina2', function () {
-    return'Estás en la pagina 2';
+ Route::get('usuario/{id}', function ($id) {
+    echo 'Usuario' .$id;
  });
+
+ Route::get('user/{name?}', function($name = null)
+{
+return $name;
+});
+// También podemos poner algún valor por defecto...
+Route::get('user/{name?}', function($name = 'Pablo')
+{
+return $name;
+});
